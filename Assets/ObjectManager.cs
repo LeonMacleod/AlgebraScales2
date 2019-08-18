5a3acb137012bd59c096435a01737724ff648b9b
Leon Macleod <leondeanmacleod@gmail.com>
2019-08-19 11:21:54 +1200

    commit

---
+++ Assets/ObjectManager.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+
+
+    private GameObject scale1;
+    private GameObject scale2;
+
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+
+
+
+        //dealing with the first scale or object on top of it or object on top of the object on top of it.
+
+
+
+        if(collision.gameObject.name == "scale1")
+        {
+            if (this.gameObject.transform.Find("state").tag == "notadded")
+            {
+                Debug.Log(this.gameObject.name + " has collided with scale1");
+                this.gameObject.transform.Find("id").tag = "scale1";
+                this.gameObject.transform.Find("state").tag = "added";
+                scale1.GetComponent<scale>().attachedBodies.Add(this.gameObject);
+            }
+
+
+            
+        }
+        else
+        {
+
+            if(collision.gameObject.transform.Find("id") != null)
+            {
+                if (collision.gameObject.transform.Find("id").tag == "scale1")
+                {
+
+                    if (this.gameObject.transform.Find("state").tag == "notadded")
+                    {
+                        Debug.Log(this.gameObject.name + " has collided with scale1");
+                        this.gameObject.transform.Find("id").tag = "scale1";
+                        this.gameObject.transform.Find("state").tag = "added";
+                        scale1.GetComponent<scale>().attachedBodies.Add(this.gameObject);
+
+                    }
+
+
+
+                }
+            }
+        
+            
+
+
+        }
+
+
+
+
+
+        
+
+
+        if (collision.gameObject.name == "scale2")
+        {
+            if (this.gameObject.transform.Find("state").tag == "notadded")
+            {
+                Debug.Log(this.gameObject.name + " has collided with scale2");
+                this.gameObject.transform.Find("id").tag = "scale2";
+                this.gameObject.transform.Find("state").tag = "added";
+                scale2.GetComponent<scale>().attachedBodies.Add(this.gameObject);
+            }
+
+
+
+        }
+        else
+        {
+
+            if(collision.gameObject.transform.Find("id") != null)
+            {
+
+                if (collision.gameObject.transform.Find("id").tag == "scale2")
+                {
+
+                    if (this.gameObject.transform.Find("state").tag == "notadded")
+                    {
+                        Debug.Log(this.gameObject.name + " has collided with scale2");
+                        this.gameObject.transform.Find("id").tag = "scale2";
+                        this.gameObject.transform.Find("state").tag = "added";
+                        scale2.GetComponent<scale>().attachedBodies.Add(this.gameObject);
+
+                    }
+
+
+
+                }
+            }
+            
+
+
+        }
+        
+
+
+
+    }
+
+       
+
+
+
+        //dealing with the second scale or object on top of it or object on top of the object on top of it.
+
+        /*
+        
+        if (collision.gameObject.name == "scale2" || collision.gameObject.transform.Find("id").tag == "scale2")
+        {
+
+            if (this.gameObject.transform.Find("state").tag == "notadded")
+            {
+                Debug.Log(this.gameObject.name + " has collided with scale2");
+                this.gameObject.transform.Find("id").tag = "scale2";
+                this.gameObject.transform.Find("state").tag = "added";
+                scale2.GetComponent<scale>().attachedBodies.Add(this.gameObject);
+            }
+
+
+
+
+
+        }
+        
+        */
+
+    
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+      
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale1 = GameObject.Find("scale1");
+        scale2 = GameObject.Find("scale2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scale.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class scale : MonoBehaviour
+{
+
+    public List<GameObject> attachedBodies;
+    public TextMeshPro massText;
+
+
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!(attachedBodies.Contains(collision.gameObject)))
+        {
+            if(collision.gameObject.tag == "CanScale")
+            {
+                attachedBodies.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (attachedBodies.Contains(collision.gameObject))
+        {
+            if(collision.gameObject.tag == "CanScale")
+            {
+                attachedBodies.Remove(collision.gameObject);
+            }
+        }
+    }*/
+
+
+    public float getMassText()
+    {
+        float mass = 0;
+        foreach (GameObject body in attachedBodies)
+        {
+            mass += body.GetComponent<Rigidbody>().mass;
+
+        }
+
+        return mass;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        massText.text = getMassText().ToString();
+    }
+}
